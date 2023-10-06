5446f1dd8401f8ee620482433949d4f69b39ceb7
Latumi1192 <honghai.bui@haw-hamburg.de>
2023-10-06 15:40:44 +0200

    Added Leasing options

--- src/features/presentation/components/LeasingDropdown.tsx
+++ src/features/presentation/components/LeasingDropdown.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import { Box } from "@mui/material";
+import { Box, ToggleButton, ToggleButtonGroup } from "@mui/material";
 import { CarServiceImpl } from "@/features/domain/services/CarServiceImpl";
 import { BrandServiceImpl } from "@/features/domain/services/BrandServiceImpl";
 import { Car } from "@/features/domain/dto/CarDTO";
@@ -26,13 +26,34 @@ export default function BasicMenu() {
   const handleClose1 = () => {
     setAnchorEl1(null);
     setCar("Select Car");
+    setVisibility(false);
   };
   const handleClose2 = () => {
     setAnchorEl2(null);
   };
+
+  const [alignment, setAlignment] = React.useState<string | null>("left");
+
+  const handleAlignment = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string | null
+  ) => {
+    setAlignment(newAlignment);
+  };
+
+  const [alignment2, setAlignment2] = React.useState<string | null>("left");
+
+  const handleAlignment2 = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string | null
+  ) => {
+    setAlignment2(newAlignment);
+  };
+
   const [brand, setBrand] = React.useState<String>("Select Brand");
   const [car, setCar] = React.useState<String>("Select Car");
   const [carArray, setCarArray] = React.useState<Car[]>([]);
+  const [visibility, setVisibility] = React.useState(false);
 
   return (
     <Box>
@@ -67,7 +88,6 @@ export default function BasicMenu() {
           </MenuItem>
         ))}
       </Menu>
-
       <Button
         id="basic-button"
         aria-controls={open2 ? "basic-menu" : undefined}
@@ -91,6 +111,7 @@ export default function BasicMenu() {
           <MenuItem
             onClick={() => {
               setCar(item.carname);
+              setVisibility(true);
               handleClose2();
             }}
           >
@@ -98,6 +119,58 @@ export default function BasicMenu() {
           </MenuItem>
         ))}
       </Menu>
+      {visibility && (
+        <div>
+          <Box>
+            <ToggleButtonGroup
+              value={alignment}
+              exclusive
+              onChange={handleAlignment}
+              aria-label="text alignment"
+            >
+              <ToggleButton value="12" aria-label="left aligned">
+                12 Months
+              </ToggleButton>
+              <ToggleButton value="24" aria-label="centered">
+                24 Months
+              </ToggleButton>
+              <ToggleButton value="36" aria-label="right aligned">
+                36 Months
+              </ToggleButton>
+              <ToggleButton value="48" aria-label="justified">
+                48 Months
+              </ToggleButton>
+              <ToggleButton value="60" aria-label="justified">
+                60 Months
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+          <Box>
+            <ToggleButtonGroup
+              value={alignment2}
+              exclusive
+              onChange={handleAlignment2}
+              aria-label="text alignment"
+            >
+              <ToggleButton value="12" aria-label="left aligned">
+                10.000 km
+              </ToggleButton>
+              <ToggleButton value="24" aria-label="centered">
+                15.000 km
+              </ToggleButton>
+              <ToggleButton value="36" aria-label="right aligned">
+                20.000 km
+              </ToggleButton>
+              <ToggleButton value="48" aria-label="justified">
+                25.000 km
+              </ToggleButton>
+              <ToggleButton value="60" aria-label="justified">
+                30.000 km
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+        </div>
+      )}
     </Box>
   );
 }
